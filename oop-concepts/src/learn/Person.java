a953fec37c838b44826a2211a115c6908f262dd1
ricky-blip <ricky.rinaldy77@gmail.com>
2025-05-16 21:48:02 +0700

    update oop-concepts, learn class, create object, create field, and use field in another class

---
+++ oop-concepts/src/learn/Person.java
@@ -0,0 +1,9 @@
+package learn;
+
+//Person is 'Class'
+public class Person {
+    //membuat field/atribute
+    String name;
+    String address;
+    final String country = "Germany";
+}
---
+++ oop-concepts/src/learn/PersonApp.java
@@ -0,0 +1,17 @@
+package learn;
+
+public class PersonApp {
+    //Membuat Object
+    public static void main(String[] args) {
+        var person1 = new Person();
+        Person person2 = new Person();
+        Person person3;
+        person3 = new Person();
+
+        var name = person1.name = "Ricky";
+        var address = person1.address = "Palembang";
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(person1.country);
+    }
+}
